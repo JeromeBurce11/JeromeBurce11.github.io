d03601fc60f8cadddb31eb08010b77d83394cd3b
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-10-09 13:53:39 +0800

    midterm exam

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -2,63 +2,69 @@
 
 // basic functionalities
 $(document).ready(function () {
-  var topic1= $("input[name='topic']").val();
-  $("#btn-disconnect").click(function () {
-    client.end();
-    alert("session ended");
-    $("#status").val("Disconnected !");
-    location.reload();
+
+  // var topic1 = $("input[name='topic']").val();
+  // $("#btn-disconnect").click(function () {
+  //   client.end();
+  //   alert("session ended");
+  //   $("#status").val("Disconnected !");
+  //   location.reload();
+  // })
+  // $("#btn-connect").click(function () {
+
+  client = mqtt.connect('wss://test.mosquitto.org:8081/mqtt')
+
+  client.on("connect", function () {
+    $("#status").val("Connected !");
+    console.log("successfully connected");
+    $("#Onbutton").click(function () {
+
+      var topic = 'burce/device/status';
+      var payload = 'Turn On:' + moment().format('MMMM Do YYYY, h:mm:ss a');
+      $("#Sstatus").val('The device is on');
+      client.publish(topic, payload);
+    });
+
+    $("#Offbutton").click(function () {
+      var topic = 'burce/device/status';
+      var payload = 'Turn off:' + moment().format('MMMM Do YYYY, h:mm:ss a');
+      $("#Sstatus").val('The device is Off');
+      client.publish(topic, payload);
+    });
   })
-  $("#btn-connect").click(function () {
 
-    client = mqtt.connect($("#address").val())
-   
-    client.on("connect", function () {
-      $("#status").val("Connected !");
-      console.log("successfully connected");
-    })
-    subs=false;
-    $("#btn-publish").click(function(){
-    
-      var topic= $("input[name='topic']").val();
-      var payload = $("input[name='payload']").val();
-      var row = "<tr><td>"+ topic +"</td><td>"+ payload+"</td><td>"+moment().format('MMMM Do YYYY, h:mm:ss a')+ "</td></tr>";
-      $("#tbpublish").append(row);
-      subs=true;
 
-      client.publish(topic, payload)
-    })
-    
-    $("#btn-subscribe").click(function(){
-      var topic=$("input[name='topicSub']").val();
-      var row = "<tr><td>"+ topic +"</td><td>"+moment().format('MMMM Do YYYY, h:mm:ss a')+ "</td></tr>";
-      $("#tbsubscribe").append(row);
-      $("#btn-publish").click(function(){
-        var payload = $("input[name='payload']").val();
-        if(topic==topic1){
-          var row = "<tr><td>"+ topic +"</td><td>"+ payload+"</td><td>"+moment().format('MMMM Do YYYY, h:mm:ss a')+ "</td></tr>";
-          $("#tbbroker").append(row);
-        }
-        
-      });
-      topic1 =  $("input[name='topic']").val();
-      
-      client.subscribe(topic)
-      client.on("message", function (topic, payload) {
-        console.log([topic, payload].join(": "));
-       var row = "<tr><td>"+ topic +"</td><td>"+ payload+"</td><td>"+moment().format('MMMM Do YYYY, h:mm:ss a')+ "</td></tr>";
-       $("#tbbroker").append(row);   
-      })
-      
-    })
-    $("#btn-unsubscribe").click(function(){
-      var topic=$("input[name='topicSub']").val();
-      client.unsubscribe(topic)
-      topic1= "";
 
-    })
+  // $("#btn-subscribe").click(function () {
+  //   var topic = $("input[name='topicSub']").val();
+  //   var row = "<tr><td>" + topic + "</td><td>" + moment().format('MMMM Do YYYY, h:mm:ss a') + "</td></tr>";
+  //   $("#tbsubscribe").append(row);
+  //   $("#btn-publish").click(function () {
+  //     var payload = $("input[name='payload']").val();
+  //     if (topic == topic1) {
+  //       var row = "<tr><td>" + topic + "</td><td>" + payload + "</td><td>" + moment().format('MMMM Do YYYY, h:mm:ss a') + "</td></tr>";
+  //       $("#tbbroker").append(row);
+  //     }
 
-  })
+  //   });
+  //   topic1 = $("input[name='topic']").val();
+
+  //   client.subscribe(topic)
+  //   client.on("message", function (topic, payload) {
+  //     console.log([topic, payload].join(": "));
+  //     var row = "<tr><td>" + topic + "</td><td>" + payload + "</td><td>" + moment().format('MMMM Do YYYY, h:mm:ss a') + "</td></tr>";
+  //     $("#tbbroker").append(row);
+  //   })
+
+  // })
+  // $("#btn-unsubscribe").click(function () {
+  //   var topic = $("input[name='topicSub']").val();
+  //   client.unsubscribe(topic)
+  //   topic1 = "";
+
+  // })
+
+  // })
 })
 
 
