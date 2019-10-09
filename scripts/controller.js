76c34ab0cc8353b77434c6dafb799e2945e4480b
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-10-09 18:27:21 +0800

    6:22 10/9/2019

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,15 +3,6 @@
 // basic functionalities
 $(document).ready(function () {
 
-  // var topic1 = $("input[name='topic']").val();
-  // $("#btn-disconnect").click(function () {
-  //   client.end();
-  //   alert("session ended");
-  //   $("#status").val("Disconnected !");
-  //   location.reload();
-  // })
-  // $("#btn-connect").click(function () {
-
   client = mqtt.connect('wss://test.mosquitto.org:8081/mqtt')
 
   client.on("connect", function () {
@@ -34,37 +25,6 @@ $(document).ready(function () {
   })
 
 
-
-  // $("#btn-subscribe").click(function () {
-  //   var topic = $("input[name='topicSub']").val();
-  //   var row = "<tr><td>" + topic + "</td><td>" + moment().format('MMMM Do YYYY, h:mm:ss a') + "</td></tr>";
-  //   $("#tbsubscribe").append(row);
-  //   $("#btn-publish").click(function () {
-  //     var payload = $("input[name='payload']").val();
-  //     if (topic == topic1) {
-  //       var row = "<tr><td>" + topic + "</td><td>" + payload + "</td><td>" + moment().format('MMMM Do YYYY, h:mm:ss a') + "</td></tr>";
-  //       $("#tbbroker").append(row);
-  //     }
-
-  //   });
-  //   topic1 = $("input[name='topic']").val();
-
-  //   client.subscribe(topic)
-  //   client.on("message", function (topic, payload) {
-  //     console.log([topic, payload].join(": "));
-  //     var row = "<tr><td>" + topic + "</td><td>" + payload + "</td><td>" + moment().format('MMMM Do YYYY, h:mm:ss a') + "</td></tr>";
-  //     $("#tbbroker").append(row);
-  //   })
-
-  // })
-  // $("#btn-unsubscribe").click(function () {
-  //   var topic = $("input[name='topicSub']").val();
-  //   client.unsubscribe(topic)
-  //   topic1 = "";
-
-  // })
-
-  // })
 })
 
 
