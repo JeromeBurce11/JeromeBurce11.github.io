5bb7537006159bdee3d7e68b9614c6611e23cf36
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-10-09 14:16:11 +0800

    4545

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
  });
 
-// app.use(express.static('public'));
-app.use("/static", express.static('./scripts/'));
+ app.use(express.static('public'));
+//app.use("/static", express.static('./scripts/'));
 http.listen(port, function(){
   console.log('listening on *: ' + port);
 });
