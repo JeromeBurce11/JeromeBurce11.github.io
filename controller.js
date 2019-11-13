3c7fc857b07843c69183ce8b282b41f3ff13f013
JeromeBurce <54094084+JeromeBurce11@users.noreply.github.com>
2019-11-13 13:29:52 +0800

    Update controller.js

--- controller.js
+++ controller.js
@@ -10,15 +10,17 @@ $(document).ready(function () {
     console.log("successfully connected");
     $("#Onbutton").click(function () {
 
-      var topic = 'burce/device/status';
-      var payload = 'Turn On:' + moment().format('MMMM Do YYYY, h:mm:ss a');
+      var topic = 'group1/laser';
+      var payload = 'on';
+      //var payload = 'Turn On:' + moment().format('MMMM Do YYYY, h:mm:ss a');
       $("#Sstatus").val('The device is on');
       client.publish(topic, payload);
     });
 
     $("#Offbutton").click(function () {
-      var topic = 'burce/device/status';
-      var payload = 'Turn off:' + moment().format('MMMM Do YYYY, h:mm:ss a');
+      var topic = 'group1/laser';
+        var payload = 'off';
+      //var payload = 'Turn off:' + moment().format('MMMM Do YYYY, h:mm:ss a');
       $("#Sstatus").val('The device is Off');
       client.publish(topic, payload);
     });
